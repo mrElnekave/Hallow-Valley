3aa07726bb76686bd608ffceb66dc1f3dd109abe
mrElnekave <ye41550@gmail.com>
2022-01-18 20:56:42 -0800

    loading and setting objects

--- Game/objects.py
+++ Game/objects.py
@@ -1,8 +1,83 @@
+# from tkinter.messagebox import NO
 import pygame
 pygame.init()
 from constants import *
 
 display = pygame.Surface((WINDOWWIDTH, WINDOWHEIGHT))
+
+# game logic
+gamestate = 0
+daytime = True
+freeze = False
+shopShowing = False
+
+
+# things in game
+abilities = [None, None, None, None, None, None, None, None, None, None]
+abilityPanel = []
+levels = [1,1,1,1,1,1,1,1]
+quests = []
+chunks = []
+player = None
+currentChunk = None
+
+
+# player resources
+resourceAmounts = {
+    "coins": 1000000, 
+    "ghostEnergy": 0
+    }
+potions = {
+    "purple": 0, 
+    "red": 0, 
+    "blue": 0, 
+    "gold": 0
+}
+
+
+# runnables
+potionEffects = {
+    "purple":["objects.player.currentHealth += objects.player.maxHealth * .2","objects.resourceAmounts['ghostEnergy'] += objects.player.maxHealth * .2"],
+    "red":["objects.player.currentHealth = objects.player.maxHealth"],
+    "blue":["objects.resourceAmounts['ghostEnergy'] = objects.player.maxEnergy"],
+    "gold":["objects.player.currentHealth = objects.player.maxHealth","objects.resourceAmounts['ghostEnergy'] = objects.player.maxEnergy"],
+}
+
+# quests
+"""#"Find the Fire Key! ","Defeat the Fire Ghost! ","Find the Ice Key! ","Defeat the Ice Ghost! ","Find the Lightning Key! ","Defeat the Lightning Ghost! ","Find the Poison Key! ","Defeat the Poison Ghost! ","Find the Summoning Key! ","Defeat the Summoner Ghost! ","Find the Shield Key! ","Defeat the Shield Ghost! ","Find the Laser Key! ","Defeat the Laser Ghost! ","Find the Water Key! ","Defeat the Water Ghost! ","Find the Boss Key! ","Defeat the Dark Ghost! "]
+currentQuest = 0
+achievements = [
+    "Buy 3 gold potions!",
+    "Defeat 100 enemies!",
+    "Travel the entire map! ",
+    "Defeat 5 fire ghosts!",
+    "Survive for 10 days!",
+    "Take 1000 damage (careful with your health)!",
+    "Have infinite health for 3 minutes overall!",
+    "Defeat 5 enemies in one electrodash!",
+    "Use the laser arrows at least 50 times! ", 
+    "Upgrade one aspect of the player to the highest level! ", 
+    "Buy the highest level upgrade of every ability! ", 
+    "Pick up 100 Question Cubes!", 
+    "PLEASE ENTER MORE HERE"
+    ]""" # TODO: implement achievments
+
+
+# math
+problems = []
+
+# reset
+def Reset():
+    player.currentHealth = player.maxHealth
+    resourceAmounts["ghost energy"] = 0
+    player.chunk = (0, 0)
+    currentChunk = chunks[player.chunk[1]][player.chunk[0]]
+    player.rect.topleft = (0,0)
+    for chunkList in chunks:
+        for chunk in chunkList:
+            for thing in chunk.contents:
+                if thing.type == "enemy": 
+                    thing.health = thing.maxHealth
 
 
 '''
