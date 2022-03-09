66aa493c7ebe265c6d0f550fc348ee0c0d1bd783
Qwerty32768 <94502697+Qwerty32768@users.noreply.github.com>
2022-03-08 18:16:23 -0800

    Update fib.py

--- practice/fib.py
+++ practice/fib.py
@@ -11,6 +11,20 @@ def fib(a, b):
 # [1] + [1] + [2] + [3] + fib(5, 8)...
 # fib(1, 1)
 
+# Yuan's code
+def fibonacci(a,b,level,listThing): 
+    if level > 0: 
+        print(a) 
+        listThing.append(a)
+        fibonacci(b,a+b,level-1,listThing)
+    else: 
+        print(listThing)
+def ex13(): 
+    outputList = []
+    number = int(input("Enter a number: "))
+    fibonacci(1,1,number,outputList)
+
+# Max's code
 def fib(a, b, times_left=10):
     if times_left != 0:
         return [a] + fib(b, a+b, times_left-1)
@@ -19,3 +33,4 @@ def fib(a, b, times_left=10):
         return []
 
 print(fib(1, 1))
+
