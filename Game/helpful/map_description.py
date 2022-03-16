d449f664c1c93a50cdf417e2680e3a63b30e4412
Yamm Elnekave <klavlavigugu@gmail.com>
2022-03-15 18:46:17 -0700

    starting loadmap

--- Game/helpful/map_description.py
+++ Game/helpful/map_description.py
@@ -1,3 +1,4 @@
+import images
 map=[
     [0,0,1,1,1,1,6,6,6,6,6,6,6,6,6],
     [0,0,1,1,1,1,6,6,6,6,6,6,6,6,6],
@@ -27,3 +28,44 @@ portalLocations = {
     "water":[(13,14),(350,350)],
     "final":[(7,7),(250,250)]
 }
+
+location_log = {
+    0 : "town",
+    1 : "fire",
+    2 : "ice",
+    3 : "lightning",
+    4 : "poison",
+    5 : "summoner",
+    6 : "shield",
+    7 : "laser",
+    8 : "water",
+    9 : "final"
+}
+
+color_meaning_by_chunk = [
+    [gray, Obstacle],
+]
+
+map = images.demo_map
+
+
+
+def look_at(start_x, start_y, chunk_type):
+    end_x = start_x + 10
+    end_y = start_y + 10
+    for i in range(start_x, end_x):
+        for j in range(start_y, end_y):
+            color_of_pixel = map.get_at((i, j))
+
+
+
+for i in range(len(map)):
+    row = map[i]
+    start_y = 1 + 12 * i
+
+    for j in range(len(row)):
+        chunk_type = row[j]
+        start_x = 1 + 12 * j
+
+        look_at(start_x, start_y, chunk_type)
+        
