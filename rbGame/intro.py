fe314f1b7e1d14d31c837e4de43d043e0f6c5505
MaxGan123 <maxyiangan@gmail.com>
2022-07-05 17:34:29 -0700

    intro

---
+++ rbGame/intro.py
@@ -0,0 +1,16 @@
+import rubato as rb
+import objects
+from constants import *
+from rubato import GameObject, Display
+
+background = GameObject(pos = Display.center)
+background.add(rb.Image(rel_path="Data/Pixel Images/main_menu_bg.png"))
+start_button = GameObject(pos = Vector(250,450))
+start_button.add(img:= rb.Image(rel_path="Data/Pixel Images/StartButton.png"))
+start_button.add(rb.Button(width=img.get_size().x, height=img.get_size().y, onclick=lambda:rb.Game.scenes.set(objects.main.id)))
+about_us = GameObject(pos = Vector(480,80))
+about_us.add(img:= rb.Image(rel_path = "Data/Pixel Images/Help Button.png"))
+about_us.add(rb.Button(width=img.get_size().x, height=img.get_size().y))
+objects.intro.add(background)
+objects.intro.add(start_button)
+objects.intro.add(about_us)
--- rbGame/main.py
+++ rbGame/main.py
@@ -1,6 +1,8 @@
 from constants import *
 import objects
 import rubato as rb
+import intro
+
 
 rb.Game.scenes.set(objects.intro.id)
 
