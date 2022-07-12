4ba9c8f129db8d3cd4adaeda4d077a808642763d
Qwerty32768 <94502697+Qwerty32768@users.noreply.github.com>
2022-07-12 16:23:07 -0700

    Update collage.py

--- rbGame/collage.py
+++ rbGame/collage.py
@@ -1,14 +1,15 @@
 import rubato as rb
 import intro
 import images
+import random
+import copy
 
-
-rb.init(name="Untitled Collage")
+test_images = [images.stormCloud,images.clearCloud,images.small_bolt,images.medium_bolt,images.large_bolt]
+rb.init(name="Untitled Collage",res=rb.Vector(360,360))
 test_scene = rb.Scene(name="collage")
 rb.Game.scenes.set(test_scene.id)
 
-
-gameobj = rb.wrap(comp=images.stormCloud,pos=rb.Display.center)
-test_scene.add(gameobj)
+for _ in range(100):
+    test_scene.add(rb.wrap(comp=copy.copy(random.choice(test_images)),pos=rb.Vector(random.randint(0,rb.Display.res.x),random.randint(0,rb.Display.res.y))))
 
 rb.begin()
