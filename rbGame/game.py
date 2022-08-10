e7d88f512d4ba0f511519277cb4f4a36f432fcd0
Yamm Elnekave <klavlavigugu@gmail.com>
2022-08-09 18:38:57 -0800

    tab screen

--- rbGame/game.py
+++ rbGame/game.py
@@ -14,10 +14,23 @@ def camera_follow():
     target = objects.player.pos.clamp(Display.center, BASICLEVELSIZE - Display.center)
     rb.Game.camera.pos = rb.Game.camera.pos.lerp(target, follow_strength)
 
+def key_down():
+    if rb.Input.key_pressed("tab"):
+        objects.inTab = True
+
+        # show our tab screen, and the players position on the map
+        print("hi")
+    else:
+        objects.inTab = False
+
+def update():
+    camera_follow()
+
+
 for row in objects.chunks:
     day_night = rb.wrap(MapClasses.DayNightCycle(), name="daynight", pos=Display.center)
     for chunk in row:
-        chunk.update = camera_follow
+        chunk.update = update
         chunk.add(day_night)
 
 objects.player = classes.PlayerController(moveSpeed)
@@ -26,6 +39,9 @@ objects.player_go = player
 player.add(objects.player)
 # go = rb.wrap(comp=images.maps[0][0], pos=Display.res)
 # print(go)
+
+
+
 
 
 objects.main.add(player)
