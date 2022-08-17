c86443096c9bcb225054e2629ee6b9ed6bab8025
Qwerty32768 <94502697+Qwerty32768@users.noreply.github.com>
2022-08-16 19:29:23 -0700

    UI

--- rbGame/classes.py
+++ rbGame/classes.py
@@ -13,6 +13,10 @@ class PlayerController(rb.Component):
         self.image = images.player
         self.rect = self.image.get_rect()
         self.speed = speed
+        self.maxHealth = 100.00
+        self.currentHealth = self.maxHealth
+        self.energy = 0
+        self.maxEnergy = 100
 
     @property
     def pos(self):  # TODO: explain next class properly
