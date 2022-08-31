6591f40000331e4fca9f1d8db4706b4e9ec56e23
Qwerty32768 <94502697+Qwerty32768@users.noreply.github.com>
2022-08-30 19:51:46 -0700

    swapped order of location, chunk in createDungeon

--- rbGame/game.py
+++ rbGame/game.py
@@ -60,21 +60,21 @@ classes.spawn_poison(objects.chunks[0][0],rb.Vector(400,400))
 
 # ADD THE DUNGEONS
 data = map_description.portalLocations["fire"]
-MapLoader.createDungeon(1,bosses.FireGhostBoss(),data[0],data[1],images.fire_boss_bg,images.portal,"Dungeon 1")
+MapLoader.createDungeon(1,bosses.FireGhostBoss(),data[1],data[0],images.fire_boss_bg,images.portal,"Dungeon 1")
 data = map_description.portalLocations["ice"]
-MapLoader.createDungeon(2,bosses.FireGhostBoss(),data[0],data[1],images.fire_boss_bg,images.portal,"Dungeon 2")
+MapLoader.createDungeon(2,bosses.FireGhostBoss(),data[1],data[0],images.fire_boss_bg,images.portal,"Dungeon 2")
 data = map_description.portalLocations["lightning"]
-MapLoader.createDungeon(3,bosses.FireGhostBoss(),data[0],data[1],images.fire_boss_bg,images.portal,"Dungeon 3")
+MapLoader.createDungeon(3,bosses.FireGhostBoss(),data[1],data[0],images.fire_boss_bg,images.portal,"Dungeon 3")
 data = map_description.portalLocations["poison"]
-MapLoader.createDungeon(4,bosses.FireGhostBoss(),data[0],data[1],images.fire_boss_bg,images.portal,"Dungeon 4")
+MapLoader.createDungeon(4,bosses.FireGhostBoss(),data[1],data[0],images.fire_boss_bg,images.portal,"Dungeon 4")
 data = map_description.portalLocations["summoner"]
-MapLoader.createDungeon(5,bosses.FireGhostBoss(),data[0],data[1],images.fire_boss_bg,images.portal,"Dungeon 5")
+MapLoader.createDungeon(5,bosses.FireGhostBoss(),data[1],data[0],images.fire_boss_bg,images.portal,"Dungeon 5")
 data = map_description.portalLocations["shield"]
-MapLoader.createDungeon(6,bosses.FireGhostBoss(),data[0],data[1],images.fire_boss_bg,images.portal,"Dungeon 6")
+MapLoader.createDungeon(6,bosses.FireGhostBoss(),data[1],data[0],images.fire_boss_bg,images.portal,"Dungeon 6")
 data = map_description.portalLocations["laser"]
-MapLoader.createDungeon(7,bosses.FireGhostBoss(),data[0],data[1],images.fire_boss_bg,images.portal,"Dungeon 7")
+MapLoader.createDungeon(7,bosses.FireGhostBoss(),data[1],data[0],images.fire_boss_bg,images.portal,"Dungeon 7")
 data = map_description.portalLocations["water"]
-MapLoader.createDungeon(8,bosses.FireGhostBoss(),data[0],data[1],images.fire_boss_bg,images.portal,"Dungeon 8")
+MapLoader.createDungeon(8,bosses.FireGhostBoss(),data[1],data[0],images.fire_boss_bg,images.portal,"Dungeon 8")
 data = map_description.portalLocations["final"]
-MapLoader.createDungeon(9,bosses.FireGhostBoss(),data[0],data[1],images.fire_boss_bg,images.portal,"Dungeon 9")
+MapLoader.createDungeon(9,bosses.FireGhostBoss(),data[1],data[0],images.fire_boss_bg,images.portal,"Dungeon 9")
 
